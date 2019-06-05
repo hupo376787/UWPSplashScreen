34ea874b12ae67c42b6398a1907df1feea9205d6
Vincent Wang <376787823@qq.com>
2019-06-05 10:07:25 +0800

    Add files via upload

---
+++ SplashTest/SplashTest/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,9 @@
+﻿namespace SplashTest.Behaviors
+{
+    public enum NavigationViewHeaderMode
+    {
+        Always,
+        Never,
+        Minimal
+    }
+}
---
+++ SplashTest/SplashTest/Services/ActivationService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using SplashTest.Activation;
+using SplashTest.Views;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SplashTest.Services
+{
+    // For more information on application activation see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/activation.md
+    internal class ActivationService
+    {
+        private readonly App _app;
+        private readonly Type _defaultNavItem;
+        private Lazy<UIElement> _shell;
+
+        private object _lastActivationArgs;
+
+        public ActivationService(App app, Type defaultNavItem, Lazy<UIElement> shell = null)
+        {
+            _app = app;
+            _shell = shell;
+            _defaultNavItem = defaultNavItem;
+        }
+
+        public async Task ActivateAsync(object activationArgs)
+        {
+            if (IsInteractive(activationArgs))
+            {
+                // Initialize things like registering background task before the app is loaded
+                await InitializeAsync();
+
+                if ((activationArgs as LaunchActivatedEventArgs).PreviousExecutionState != ApplicationExecutionState.Running)
+                {
+                    bool loadState = ((activationArgs as LaunchActivatedEventArgs).PreviousExecutionState == ApplicationExecutionState.Terminated);
+                    ExtendedSplash extendedSplash = new ExtendedSplash((activationArgs as LaunchActivatedEventArgs).SplashScreen, loadState);
+                    var rootFrame = new Frame();
+                    rootFrame.Content = extendedSplash;
+                    Window.Current.Content = rootFrame;
+                }
+
+                // Do not repeat app initialization when the Window already has content,
+                // just ensure that the window is active
+                if (Window.Current.Content == null)
+                {
+                    // Create a Frame to act as the navigation context and navigate to the first page
+                    Window.Current.Content = _shell?.Value ?? new Frame();
+                }
+            }
+
+            await HandleActivationAsync(activationArgs);
+            _lastActivationArgs = activationArgs;
+
+            if (IsInteractive(activationArgs))
+            {
+                // Ensure the current window is active
+                Window.Current.Activate();
+
+                // Tasks after activation
+                await StartupAsync();
+            }
+        }
+
+        private async Task InitializeAsync()
+        {
+            await ThemeSelectorService.InitializeAsync();
+        }
+
+        private async Task HandleActivationAsync(object activationArgs)
+        {
+            var activationHandler = GetActivationHandlers()
+                                                .FirstOrDefault(h => h.CanHandle(activationArgs));
+
+            if (activationHandler != null)
+            {
+                await activationHandler.HandleAsync(activationArgs);
+            }
+
+            if (IsInteractive(activationArgs))
+            {
+                var defaultHandler = new DefaultLaunchActivationHandler(_defaultNavItem);
+                if (defaultHandler.CanHandle(activationArgs))
+                {
+                    await defaultHandler.HandleAsync(activationArgs);
+                }
+            }
+        }
+
+        private async Task StartupAsync()
+        {
+            await ThemeSelectorService.SetRequestedThemeAsync();
+        }
+
+        private IEnumerable<ActivationHandler> GetActivationHandlers()
+        {
+            yield break;
+        }
+
+        private bool IsInteractive(object args)
+        {
+            return args is IActivatedEventArgs;
+        }
+    }
+}
